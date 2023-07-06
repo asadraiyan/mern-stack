a8dd2cdc9ea51112e9a795ba0f75991699ffee0f
Asad Raiyan <asadraiyan001@gmail.com>
2023-07-06 11:07:58 +0530

    hashing the password using bcryptjs method

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-
+const bcrypt = require("bcryptjs")
 const userSchema = new mongoose.Schema({
     name : {
         type : String,
@@ -25,6 +25,18 @@ const userSchema = new mongoose.Schema({
         type : String,
         required: true
     }
+})
+
+// we are hashing the password (it convert the plain text into crypted form in the database)
+
+userSchema.pre("save", async function(next){
+if(this.isModified("password")){
+    this.password = await bcrypt.hash(this.password,12)
+    this.confirmPassword = await bcrypt.hash(this.confirmPassword,12)
+}
+
+next()
+
 })
 
 const User = mongoose.model("USER" , userSchema)
